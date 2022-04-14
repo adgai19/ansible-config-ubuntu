5a20146b52603eead6f4bc97a6b4d1a8b7e6020d
adgai19 <gaitondeaditya@gmail.com>
2022-04-14 16:37:34 +0530

    some more automation

---
+++ bin/dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Paths
+DOTFILES_DIR="$HOME/ansible-config-ubuntu"
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo pip install ansible
+fi
+
+if ! [[ -d "$DOTFILES_DIR" ]]; then
+  git clone "git@github.com:adgai19/ansible-config-ubuntu.git" "$DOTFILES_DIR"
+else
+  git -C "$DOTFILES_DIR" pull
+fi
+
+if ! [ -x "$(command -v dotfiles.sh)" ]; then
+  ln -s "$DOTFILES_DIR/bin/dotfiles.sh" "$HOME/.local/bin"
+fi
+cd "$DOTFILES_DIR"
+
+ansible-galaxy install -r requirements.yml
+
+ansible-playbook --diff "$DOTFILES_DIR/main.yml"
